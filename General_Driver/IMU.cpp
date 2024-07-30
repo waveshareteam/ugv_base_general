a486bf4777d891320538f7e735fd5546f19e20ac
Kerim Satirli <k.satirli@gmail.com>
2024-07-30 16:39:37 +0200

    extraneous whitespace linting

--- General_Driver/IMU.cpp
+++ General_Driver/IMU.cpp
@@ -89,7 +89,7 @@ void imuDataGet(EulerAngles *pstAngles,
 
   pstAngles->pitch = asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3; // pitch
   pstAngles->roll = atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3; // roll
-  pstAngles->yaw = atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3; 
+  pstAngles->yaw = atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3;
 
   pstGyroRawData->X = gyro[0];
   pstGyroRawData->Y = gyro[1];
@@ -102,7 +102,7 @@ void imuDataGet(EulerAngles *pstAngles,
   return;
 }
 
-void imuAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz) 
+void imuAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
 {
   float norm;
   float hx, hy, hz, bx, bz;
